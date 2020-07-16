b326954c943099400fea5a2cac825f7ace1610c7
thaoeu <154942630@qq.com>
2020-07-16 16:36:50 +0800

    Media button optimization

--- config.h
+++ config.h
@@ -1,7 +1,8 @@
 #include <X11/XF86keysym.h>
-/* See LICENSE file for copyright and license details. */
 
-/* appearance */
+/******
+appearance
+******/
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
@@ -21,17 +22,21 @@ static const char col_cyan[]        = "#192224";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
+
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 static const unsigned int alphas[][3]      = {
+	
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
-/* tagging */
+/******
+tagging
+******/
 static const char *tags[] = { "1,", "2,", "'3,", "4,", "5,", "6.", "7.'", "8.", "9" };
 
 static const Rule rules[] = {
@@ -44,7 +49,9 @@ static const Rule rules[] = {
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
-/* layout(s) */
+/******
+layout(s)
+******/
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
@@ -56,7 +63,9 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
-/* key definitions */
+/******
+key definitions
+******/
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
@@ -67,68 +76,84 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "56x35", NULL };
-static const char *termcmd[] = { "st", NULL };
-static const char *konsole[] = { "konsole", NULL };
-static const char *screenshot[] = { "deepin-screenshot", NULL };
-static const char *krunner[] = { "krunner", NULL };
-static const char *firefox[] = { "firefox", NULL };
-static const char *anki[]	 = { "anki", NULL };
-static const char *oud[]	 = { "electron-netease-cloud-music", NULL};
-static const char *vivaldi[] = { "vivaldi-stable", NULL };
-static const char *keynav[]  = { "/home/qj/Script/keynav.sh", NULL };
-static const char *volup[]	 = { "/home/qj/Script/vol-up.sh", NULL };
-static const char *voldown[] = { "/home/qj/Script/vol-down.sh", NULL };
-static const char *up1[]	 = { "/home/qj/Script/vol1u.sh", NULL };
-static const char *down1[]	 = { "/home/qj/Script/vol1d.sh", NULL };
-static const char *mute[]	 = { "/home/qj/Script/vol-toggle.sh", NULL };
-static const char *ranger[]	 = { "/home/qj/Script/st-ranger.sh", NULL };
-static const char *wpch[]	 = { "/home/qj/Script/wp-change.sh", NULL };
+/******
+commands
+******/
+static char dmenumon[2]			=	"0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]		=	{ "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char scratchpadname[] 	=	"scratchpad";
+static const char *scratchpadcmd[]	=	{ "st", "-t", scratchpadname, "-g", "56x35", NULL };
+static const char *termcmd[]		=	{ "st", NULL };
+static const char *konsole[]		=	{ "konsole", NULL };
+static const char *screenshot[]		=	{ "deepin-screenshot", NULL };
+//static const char *krunner[]		=	{ "krunner", NULL };
+static const char *firefox[]		=	{ "firefox", NULL };
+static const char *anki[]		=	{ "anki", NULL };
+static const char *oud[]		=	{ "electron-netease-cloud-music", NULL};
+static const char *vivaldi[]		=	{ "vivaldi-stable", NULL };
+
+/******
+Scripts
+******/
+static const char *keynav[]		=	{ "/home/qj/Script/keynav.sh", NULL };
+static const char *ranger[]		=	{ "/home/qj/Script/st-ranger.sh", NULL };
+static const char *WpCh[]		=	{ "/home/qj/Script/wp-change.sh", NULL };
+static const char *VolUp[]		=	{ "/home/qj/Script/vol-up.sh", NULL };
+static const char *VolDown[]		=	{ "/home/qj/Script/vol-down.sh", NULL };
+static const char *Mute[]		=	{ "/home/qj/Script/vol-toggle.sh", NULL };
+static const char *PlNext[]		=	{ "/home/qj/Script/Play-Next.sh", NULL };
+static const char *PlPrev[]		=	{ "/home/qj/Script/Play-Prev.sh", NULL };
+static const char *Pause[]		=	{ "/home/qj/Script/Play-pause.sh", NULL };
+static const char *Up1[]		=	{ "/home/qj/Script/vol1u.sh", NULL };
+static const char *Dn1[]		=	{ "/home/qj/Script/vol1d.sh", NULL };
 
 
 static Key keys[] = {
+
 	/* modifier                     key          function      argument */
-	{ MODKEY,					    XK_Return,	 spawn,        {.v = termcmd } },
-	{ MODKEY,                       XK_space,	 spawn,        {.v = dmenucmd } },
-	{ MODKEY, 						XF86XK_AudioRaiseVolume,	 spawn,		   {.v = up1 } },
-	{ 0,							XF86XK_AudioRaiseVolume,	 spawn,		   {.v = volup   } },
-	{ MODKEY,                   	XK_backslash,spawn,		   {.v = volup   } },
-	{ 0,							XF86XK_AudioLowerVolume,	 spawn,		   {.v = voldown } },
-	{ MODKEY,						XF86XK_AudioLowerVolume,    spawn,        {.v = down1 } },
-	{ MODKEY,						XK_Insert,	 spawn,        {.v = wpch   } },
-	{ 0,							XF86XK_AudioMute,	spawn, {.v = mute } } , 
-	{ MODKEY,						XK_t,		 spawn,		   {.v = konsole } },
-	{ MODKEY,                       XK_h,		focusstack,    {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_h,		zoom,          {0} },
-	{ MODKEY,                       XK_l,		focusstack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_l,		zoom,          {0} },
-	{ MODKEY,						XK_e,		spawn,		   {.v = ranger } },
-	{ MODKEY,						XK_m,		spawn,		   {.v = anki } },
-	{ MODKEY|ShiftMask,				XK_m,		spawn,		   {.v = oud } },
-	{ MODKEY,                       XK_a,		setlayout,     {.v = &layouts[1]} },
-	{ MODKEY,                       XK_o,		setlayout,     {.v = &layouts[0]} },
-	{ MODKEY,                       XK_b,       togglebar,     {0} },
-	{ MODKEY,						XK_f,		spawn,		   {.v = firefox } },
-	{ MODKEY,						XK_v,		spawn,		   {.v = vivaldi } },
-	{ MODKEY|ShiftMask,             XK_w,	    killclient,    {0} },
-	{ MODKEY,                       XK_j,		setmfact,      {.f = -0.05} },
-	{ MODKEY,                       XK_k,		setmfact,      {.f = +0.05} },
-	{ MODKEY,                       XK_Tab,     view,           {0} },
-	{ MODKEY|ShiftMask,             XK_s,	    spawn,		    {.v = screenshot } },
-	{ MODKEY,						XK_d,		spawn,			{.v = krunner } },
-	{ MODKEY,						XK_i,		spawn,			{.v = keynav } },
+	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
+	{ MODKEY,                       XK_space,	spawn,		{.v = dmenucmd } },
+	{ MODKEY,			XK_t,		spawn,		{.v = konsole } },
+	{ MODKEY,			XK_Insert,	spawn,		{.v = WpCh   } },
+	{ MODKEY,                       XK_h,		focusstack,	{.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_h,		zoom,		{0} },
+	{ MODKEY,                       XK_l,		focusstack,	{.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_l,		zoom,		{0} },
+	{ MODKEY,			XK_e,		spawn,		{.v = ranger } },
+	{ MODKEY,			XK_m,		spawn,		{.v = anki } },
+	{ MODKEY|ShiftMask,		XK_m,		spawn,		{.v = oud } },
+	{ MODKEY,                       XK_a,		setlayout,	{.v = &layouts[1]} },
+	{ MODKEY,                       XK_o,		setlayout,	{.v = &layouts[0]} },
+	{ MODKEY,                       XK_b,		togglebar,	{0} },
+	{ MODKEY,			XK_f,		spawn,		   {.v = firefox } },
+	{ MODKEY,			XK_v,		spawn,		   {.v = vivaldi } },
+	{ MODKEY|ShiftMask,             XK_w,	    	killclient,	{0} },
+	{ MODKEY,                       XK_j,		setmfact,	{.f = -0.05} },
+	{ MODKEY,                       XK_k,		setmfact,	{.f = +0.05} },
+	{ MODKEY,                       XK_Tab,     	view,           {0} },
+	{ MODKEY|ShiftMask,             XK_s,	    	spawn,		{.v = screenshot } },
+//	{ MODKEY,			XK_d,		spawn,		{.v = krunner } },
+	{ MODKEY,			XK_i,		spawn,		{.v = keynav } },
 	{ MODKEY,                       XK_u,		fullscreen,     {0} },
-	{ MODKEY,                       XK_g,       incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_g,       incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_s,       togglesticky,   {0} },
-	{ MODKEY,	              		XK_apostrophe,	togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_g,		incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_g,		incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_s,		togglesticky,   {0} },
+	{ MODKEY,			XK_apostrophe,	togglescratch,  {.v = scratchpadcmd } },
+	/******
+	Media operation
+	******/
+
+	{ 0,				XF86XK_AudioRaiseVolume,	spawn,		{.v = VolUp   } },
+	{ MODKEY,			XF86XK_AudioRaiseVolume,	spawn,		{.v = Up1 } },
+	{ 0,				XF86XK_AudioLowerVolume,	spawn,		{.v = VolDown } },
+	{ MODKEY,			XF86XK_AudioLowerVolume,	spawn,		{.v = Dn1 } },
+	{ 0,				XF86XK_AudioMute,		spawn,		{.v = Mute } } ,
+	{ 0,				XF86XK_AudioPause,		spawn,		{.v = Pause } } ,
+	{ 0,				XF86XK_AudioNext,		spawn,		{.v = PlNext } },
+	{ 0,				XF86XK_AudioPrev,		spawn,		{.v = PlPrev } },
+//	{ MODKEY,                   	XK_backslash,spawn,		   {.v = volup   } },
 
 	/*
-#>#
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
